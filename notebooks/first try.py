df794b96605ba1378b6cba49c844825092a38426
king398 <mithilsalunkhe@outlook.com>
2021-05-10 16:57:15 +0530

    d

--- notebooks/first try.py
+++ notebooks/first try.py
@@ -2,6 +2,7 @@ import tensorflow as tf
 import numpy as np
 import os
 from tensorflow.keras import mixed_precision
+from tensorflow.keras import layers
 
 physical_devices = tf.config.list_physical_devices('GPU')
 tf.config.experimental.set_memory_growth(physical_devices[0], True)
@@ -16,3 +17,17 @@ test = tf.keras.preprocessing.image_dataset_from_directory(
 	image_size=(128, 128), seed=42)
 train = train.cache()
 test = test.cache()
+input = layers.Input(shape=(128, 128, 3))
+base_model = tf.keras.applications.EfficientNetB1(include_top=True, weights="imagenet", input_tensor=input)
+model = tf.keras.models.Sequential([
+	base_model,
+	layers.Flatten(),
+	layers.Dense(80, activation="softmax")
+
+])
+opt = tf.keras.optimizers.SGD(0.03)
+model.compile(
+	optimizer=opt,
+	loss="categorical_crossentropy",
+	metrics=['categorical_accuracy'])
+history = model.fit(train, validation_data=test, epochs=5)
